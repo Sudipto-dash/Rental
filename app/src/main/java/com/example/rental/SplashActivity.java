b32cfcd26a45a5280fa3f64efcecdd5ff99fe919
Sudipto-dash <sudiptodsh@gmail.com>
2022-12-02 02:45:40 +0600

    1st

--- app/src/main/java/com/example/rental/SplashActivity.java
+++ app/src/main/java/com/example/rental/SplashActivity.java
@@ -1,13 +1,13 @@
 package com.example.rental;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
