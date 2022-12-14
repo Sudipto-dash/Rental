a5a0879791a1f89311251b4842a2d028cebd7931
Sudipto-dash <sudiptodsh@gmail.com>
2022-12-15 03:03:57 +0600

    page2

--- app/src/main/java/com/example/rental/SplashActivity.java
+++ app/src/main/java/com/example/rental/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.example.rental;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -9,5 +12,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class) );
+            }
+        },2000);
     }
 }
